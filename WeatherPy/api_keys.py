10fd83f0b74fcc5d125de7fb30060128d495b1d0
Denisa <florentinadenisa@hotmail.com>
2025-04-23 10:45:35 -0600

    updates

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6b43736cb2a94148edf79268028ea939"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4eff85bd3b70482c9c6376c2b84c4a8e"
